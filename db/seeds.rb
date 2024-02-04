e4582c152d29d5df7f385bde96d80a81c66b6c41
ricardo canas <canasricardo17@gmail.com>
2024-02-04 20:25:58 +0100

    correction de la seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ data = JSON.parse(response)
 data['results'].each do |movie_data|
   title = movie_data['title']
   overview = movie_data['overview']
-  poster_url = movie_data["https://image.tmdb.org/t/p#{'backdrop_path'}"]
+  poster_url = "https://image.tmdb.org/t/p#{movie_data['backdrop_path']}"
   rating = movie_data['vote_average']
   movie = Movie.new(title: title, overview: overview, poster_url: poster_url, rating: rating)
   movie.save
